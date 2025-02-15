8ae5bfc0739b66ee58175fd188e25986931b40a4
mioe <mioe.boss@gmail.com>
2025-02-15 15:06:44 +0300

    fix: add missing proficiencyBonus

--- srd_5e_monsters.ts
+++ srd_5e_monsters.ts
@@ -124,6 +124,7 @@ function htmlParser(htmlCode: string) {
 
 export function changeProperties(input: IInput): IEntity {
 	let entity = {
+		proficiencyBonus: '',
 		sourceLink: 'https://gist.githubusercontent.com/tkfu/9819e4ac6d529e225e9fc58b358c3479/raw/d4df8804c25a662efc42936db60cfbc0a5b19db8/srd_5e_monsters.json',
 		strength: { score: '', modifier: '' },
 		dexterity: { score: '', modifier: '' },
